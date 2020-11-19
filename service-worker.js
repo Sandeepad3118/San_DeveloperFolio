d6a5698da8a01500ba194951be545c607a4c5f1a
Sandeepad3118 <48762505+Sandeepad3118@users.noreply.github.com>
2020-11-19 04:23:51 +0000

    Deploying to gh-pages from  @ eb59aa755dcbc9a6f63236d2edc8d6e5daf83ca8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/San_devFolio/precache-manifest.fbab53d6cab6373556abeb2e11f8148c.js"
+  "/precache-manifest.fdff7d0b5b506c8873ba85ebe47a0efe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/San_devFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
